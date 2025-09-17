564469947c8f9829867f1adad945c1ce90f0cb48
seho <sehoho1018@gmail.com>
2025-09-17 15:28:56 +0900

    python study

---
+++ ex2/ex2-3.py
@@ -0,0 +1,23 @@
+import turtle as t
+
+radius = 50
+
+t.up()
+t.goto(0,0)
+t.down()
+
+t.circle(radius)
+
+t.up()
+t.goto(100,0)
+t.down()
+
+t.circle(radius+20)
+
+t.up()
+t.goto(200,0)
+t.down()
+
+t.circle(radius+40)
+
+t.done()
